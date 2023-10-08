8d44f4e21449cee4bd1a477fb72464e33961a30e
Vidit Gupta <117505027+vidit-04@users.noreply.github.com>
2023-10-08 22:58:45 +0530

    Add files via upload

---
+++ lecture_5I.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+
+    // int a[5];
+    int a,b,c,d,e;
+    cout<<"Give 5 integers as input"<<endl;
+    cin>>a>>b>>c>>d>>e;
+    cout<<"integers in reverse order"<<e<<d<<c<<b<<a;
+
+}
